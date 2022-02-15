2badca112d1adc6a3798985f574c265f4fbae694
Nathan Merrill <nmerrill5@gmail.com>
2022-02-15 07:27:35 -0700

    Added ProjectsModule

---
+++ server/modules/projects.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsController } from 'server/controllers/projects.controller';
+import { Project } from 'server/entities/project.entity';
+import { ProjectMember } from 'server/entities/project_member.entity';
+import { Task } from 'server/entities/task.entity';
+import { UsersService } from 'server/providers/services/users.service';
+import { ProjectsService } from 'server/providers/services/projects.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Project, Task, ProjectMember])],
+  // controllers: [ProjectsController],
+  providers: [ProjectsService],
+  exports: [TypeOrmModule],
+})
+export class ProjectsModule {}
