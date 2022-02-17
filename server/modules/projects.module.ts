856f1a8a3056d107efb6f808d45d3c5f1d15cff6
Alex Thurston <alexanderthurston@gmail.com>
2022-02-17 15:57:43 -0700

    Listed TasksService as a provider

--- server/modules/projects.module.ts
+++ server/modules/projects.module.ts
@@ -5,11 +5,12 @@ import { Project } from 'server/entities/project.entity';
 import { ProjectMember } from 'server/entities/project_member.entity';
 import { Task } from 'server/entities/task.entity';
 import { ProjectsService } from 'server/providers/services/projects.service';
+import { TasksService } from 'server/providers/services/tasks.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Project, Task, ProjectMember])],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [ProjectsService, TasksService],
   exports: [TypeOrmModule],
 })
 export class ProjectsModule {}
