a2ad21944d21c3fd9cd71fb9db8a73eea2a89300
Alex Thurston <alexanderthurston@gmail.com>
2022-02-22 17:17:10 -0700

    Display assignee email

--- client/components/project_page/task.jsx
+++ client/components/project_page/task.jsx
@@ -45,7 +45,7 @@ export const Task = ({ task, setIncompleteTasks, setDoneTasks, allTasks, deleteT
       <p className="break-words">{task.description}</p>
       <p>Time: {task.timeEstimation}</p>
       <p>Status: {task.status}</p>
-      <p>User: {task.userId}</p>
+      <p>Assignee Email: {task.user.email}</p>
       <div className="py-2">
         <RedButton onClick={() => deleteTask(task)}>Delete</RedButton>
         <Button onClick={() => updateTaskAssignment(user.id)}>Assign to me</Button>
